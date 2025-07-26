a375ab5477427e9226e8d353220a02131d0c969a
shanurr02 <shanurr08@mgail.com>
2025-07-26 05:35:20 +0530

    Fix: add lucide-react dependency

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
         {/* Desktop Menu */}
         <ul className="hidden md:flex space-x-8 text-sm font-medium text-gray-700">
           <li>
-            <Link href="/" className="hover:text-black font-semibold">Home</Link>
+            <Link href="/" className="hover:text-black font-semibold">Rome</Link>
           </li>
           <li>
             <Link href="/services" className="hover:text-black">Services</Link>
